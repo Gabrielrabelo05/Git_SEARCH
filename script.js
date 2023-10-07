6f3a9e5eeb8a208d423cc827785e5a94197340d9
Gabrielrabelo05 <sergio.gpr05@gmail.com>
2023-10-07 00:32:41 -0300

    api-block test

--- script.js
+++ script.js
@@ -4,47 +4,52 @@ let input = document.querySelector("#input")
 const user = document.querySelector('#title_user')
 const p = document.querySelector("#descrition")
 
-btn.addEventListener("click", ()=> {
+btn.addEventListener("click", () => {
+    contai.style.display = "none"
     if (input.value == "") {
         alert("Preencha o campo")
-    }else {
-        contai.style.display="flex"
-    }   
+    }
 })
 function searchUser() {
-    const input =  document.querySelector ("#input").value
+    const input = document.querySelector("#input").value
     const apiUrl = `https://api.github.com/users/${input}`
+    
 
     fetch(apiUrl)
         .then(res => res.json())
         .then(data => {
-            const avatar  = data.avatar_url
+
+            contai.style.display = "flex"
+
+            const avatar = data.avatar_url
             user.textContent = data.name
-            p.textContent =  data.bio
-            const reposUrl =  data.repos_url
+            p.textContent = data.bio
+            const reposUrl = data.repos_url
             document.getElementById("avatar").src = avatar;
 
             fetch(reposUrl)
-            .then(response => response.json())
-            .then(repos => {
-                const reposList = document.getElementById("repos")
-                reposList.innerHTML = ''
-                repos.forEach(repo => {
-                    const li = document.createElement("li")
-                    const repoLink =  document.createElement("a")
-                    repoLink.href = repo.html_url
-                    repoLink.textContent = repo.name
-                    repoLink.target = "_black"
-                    li.appendChild(repoLink)
-                    reposList.appendChild(li)
+                .then(response => response.json())
+                .then(repos => {
+                    const reposList = document.getElementById("repos")
+                    reposList.innerHTML = ''
+                    repos.forEach(repo => {
+                        const li = document.createElement("li")
+                        const repoLink = document.createElement("a")
+                        repoLink.href = repo.html_url
+                        repoLink.textContent = repo.name
+                        repoLink.target = "_black"
+                        li.appendChild(repoLink)
+                        reposList.appendChild(li)
+                    })
                 })
-            })
-            .catch(error => {
-                console.error("Erro na busca do repositório", error)
-        })
-        .catch(erro => {
-            console.error("Erro na busca do usuário", erro)
+                .catch(error => {
+                    alert("Usuário não encontrado")
+                    console.error("Erro na busca do repositório", error)
+                })
 
         })
-    }
-)}
+        .catch(erro => {
+            alert("Usuário não encontrado")
+            console.error("Erro na busca do usuário", erro)
+        })
+}
